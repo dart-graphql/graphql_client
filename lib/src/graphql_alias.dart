bb830c8f0e236ed0317f256e69d2d62d7e7442c9
Thomas Hourlier <hourliert@gmail.com>
2017-08-24 18:31:45 +0200

    chore(*): enforce linter rules bis

--- lib/src/graphql_alias.dart
+++ lib/src/graphql_alias.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file.
 
+//ignore_for_file: avoid_setters_without_getters
+
 part of graphql_client.definitions;
 
 abstract class Alias implements GQLField {
