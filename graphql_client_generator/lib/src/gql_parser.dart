fa152d0c7725a808d50d25d7529d57bc9407f6e5
Thomas Hourlier <hourliert@gmail.com>
2017-09-07 23:14:59 +0200

    feat(generator): generator nested gql fields. [skip ci]
    TODO: generate operations

--- graphql_client_generator/lib/src/gql_parser.dart
+++ graphql_client_generator/lib/src/gql_parser.dart
@@ -13,37 +13,114 @@ class GQLParser {
     return parseOperation(document.definitions.first);
   }
 
-  List<Spec> parseOperation(OperationDefinitionContext query,
-      [List<Spec> specs = const []]) {
-    List<Spec> gqlDefinitions = [];
+  List<Spec> parseOperation(OperationDefinitionContext query) {
+    final gqlDefinitions = parseSelections(query.selectionSet.selections, []);
 
-    return parseSelection(query.selectionSet.selections);
+    return []..addAll(gqlDefinitions);
   }
 
-  List<Spec> parseSelection(List<SelectionContext> selections,
-      [List<Spec> specs = const []]) {
-    return selections.map((s) => parseFinalResolver(s.field)).toList();
+  List<Spec> parseSelections(
+      List<SelectionContext> selections, List<Spec> specs) {
+    selections.forEach((s) {
+      final field = s.field;
+
+      if (isScalarFieldResolver(field)) {
+        specs.add(parseField(field));
+      } else {
+        specs
+          ..add(parseField(field))
+          ..addAll(parseSelections(field.selectionSet.selections, []));
+      }
+    });
+
+    return specs;
   }
 
-  Spec parseFinalResolver(FieldContext field) {
-    if (field.selectionSet != null &&
-        field.selectionSet.selections.isNotEmpty) {
-      throw new StateError('The given field is not scalar');
+  Spec parseField(FieldContext field) {
+    final resolverName = getResolverName(field);
+    final resolverMixin = getResolverMixin(field);
+    final resolverMethods = getResolverMethods(field);
+    final resolverFields = getResolverFields(field);
+
+    return new Class((b) => b
+      ..name = resolverName
+      ..extend = const Reference('Object')
+      ..mixins.addAll(resolverMixin)
+      ..implements.add(const Reference('GQLField'))
+      ..methods.addAll(resolverMethods)
+      ..fields.addAll(resolverFields));
+  }
+
+  bool isScalarFieldResolver(FieldContext field) =>
+      field.selectionSet == null || field.selectionSet.selections.isEmpty;
+
+  String getFieldName(FieldContext field) =>
+      field.fieldName.alias?.name ?? field.fieldName.name;
+  String getFieldAlias(FieldContext field) => field.fieldName.alias?.alias;
+  String getFieldArguments(FieldContext field) =>
+      field.arguments.map((a) => a.toSource()).join(', ');
+  String getFieldDirectives(FieldContext field) =>
+      field.directives.map((d) => d.toSource()).join(', ');
+
+  String getResolverName(FieldContext field) {
+    final name = field.fieldName.alias?.name ?? field.fieldName.name;
+
+    return '${name[0].toUpperCase()}${name.substring(1)}Resolver';
+  }
+
+  Iterable<Field> getResolverFields(FieldContext field) {
+    if (!isScalarFieldResolver(field)) {
+      return field.selectionSet.selections.map((s) {
+        final nestedField = s.field;
+        final nestedResolverName = getResolverName(nestedField);
+
+        return new Field((b) => b
+          ..name = getFieldAlias(nestedField) ?? getFieldName(nestedField)
+          ..type = new Reference(nestedResolverName)
+          ..assignment =
+              new Code((b) => b..code = 'new $nestedResolverName()'));
+      });
     }
 
-    final name = field.fieldName.alias?.name ?? field.fieldName.name;
-    final alias = field.fieldName.alias?.alias;
-    final arguments = field.arguments.map((a) => a.toSource()).join(', ');
-    final directives = field.directives.map((d) => d.toSource()).join(', ');
+    return const [];
+  }
 
-    final resolverName = '${name[0].toUpperCase()}${name.substring(1)}Resolver';
-    final resolverMixin = [
-      const Reference('Scalar<String>'),
+  Iterable<Reference> getResolverMixin(FieldContext field) {
+    final alias = getFieldAlias(field);
+    final arguments = getFieldArguments(field);
+    final directives = getFieldDirectives(field);
+
+    return [
+      isScalarFieldResolver(field)
+          ? const Reference('Scalar')
+          : const Reference('Fields'),
       alias != null ? const Reference('Alias') : null,
       arguments.isNotEmpty ? const Reference('Arguments') : null,
       directives.isNotEmpty ? const Reference('Directives') : null,
     ].where((r) => r != null);
-    final resolverMethods = [
+  }
+
+  Iterable<Method> getResolverMethods(FieldContext field) {
+    final name = getFieldName(field);
+    final alias = getFieldAlias(field);
+    final arguments = getFieldArguments(field);
+    final directives = getFieldDirectives(field);
+    final resolverName = getResolverName(field);
+    final resolverFields = getResolverFields(field);
+
+    final resolverFieldsNames = resolverFields.map((f) => f.name);
+    final resolverFieldsDeclarations = resolverFieldsNames.join(', ');
+    final cloneMethodFields = 'new $resolverName()${
+        alias != null ?
+        '..aliasId = aliasId' :
+        ''
+    }${
+        resolverFieldsNames.isNotEmpty ?
+        '..${resolverFieldsNames.map((n) => '$n = $n.clone()').join('..')}' :
+        ''
+    }';
+
+    return [
       new Method((b) => b
         ..name = 'name'
         ..type = MethodType.getter
@@ -72,22 +149,23 @@ class GQLParser {
             ..annotations.add(new Annotation(
                 (b) => b..code = new Code((b) => b..code = 'override'))))
           : null,
+      !isScalarFieldResolver(field)
+          ? new Method((b) => b
+            ..name = 'fields'
+            ..type = MethodType.getter
+            ..lambda = true
+            ..returns = const Reference('List<GQLField>')
+            ..body = new Code((b) => b..code = "[$resolverFieldsDeclarations]")
+            ..annotations.add(new Annotation(
+                (b) => b..code = new Code((b) => b..code = 'override'))))
+          : null,
       new Method((b) => b
         ..name = 'clone'
         ..returns = new Reference(resolverName)
         ..lambda = true
-        ..body = new Code((b) => b
-          ..code =
-              'new $resolverName()${alias != null ? '..aliasId = aliasId' : ''}')
+        ..body = new Code((b) => b..code = cloneMethodFields)
         ..annotations.add(new Annotation(
             (b) => b..code = new Code((b) => b..code = 'override')))),
     ].where((m) => m != null);
-
-    return new Class((b) => b
-      ..name = resolverName
-      ..extend = const Reference('Object')
-      ..mixins.addAll(resolverMixin)
-      ..implements.add(const Reference('GQLField'))
-      ..methods.addAll(resolverMethods));
   }
 }
